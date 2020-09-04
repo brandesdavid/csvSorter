18439cc8b174a67f2f10525cfdd3f71e617ca4e4
david <61701882+brandesdavid@users.noreply.github.com>
2020-09-05 00:50:16 +0200

    finishing prototype v1
    added a different bubble sort, fixed some bugs, main prototype is here

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ import csv
 
 f = open('data.csv', 'r')
 
-array = []
+array = [' ']*10
 pointer = 0
 
 with f:
     reader = csv.reader(f)
     for row in reader:
         for e in row:
-          array[pointer] = e
+          array[pointer] = int(e)
           pointer += 1
 
-def bubbleSort(array):
-        length = len(array)-1
-        indexminus = 0;
-        
-        for i in range(length):
-            for j in range(0, length-indexminus):
-                if array[j] > array[j+1]:
-                    temp = array[j]
-                    array[j] = array[j+1]
-                    array[j+1] = temp
+print(str(array))
+
+def bubbleSort(arr):
+    n = len(arr) 
+  
+    
+    for i in range(n-1):
+        for j in range(0, n-i-1): 
+            if arr[j] > arr[j+1]: 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            
 
 bubbleSort(array) 
+
+print(str(array))
