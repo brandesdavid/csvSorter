23c17d02d7843fa379282d3785950693c5d18608
david <61701882+brandesdavid@users.noreply.github.com>
2020-09-05 14:27:01 +0200

    insertionSort
    added insertionSort, some small fixes

--- Algorithms.py
+++ Algorithms.py
@@ -6,3 +6,35 @@ def bubbleSort(arr):
         for j in range(0, n-i-1): 
             if arr[j] > arr[j+1]: 
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+
+def insertionSort(arr): 
+  
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
+
+def partition(arr, low, high):
+    i = (low-1)         # index of smaller element
+    pivot = arr[high]     # pivot
+ 
+    for j in range(low, high):
+ 
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+ 
+            # increment index of smaller element
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+ 
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-from Algorithms import bubbleSort
+from Algorithms import bubbleSort, insertionSort
 
 array = [' ']*100
 pointer = 0
@@ -7,16 +7,15 @@ pointer = 0
 with open('data4.csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
     
-    next(reader)
+    next(reader) # skips first line of csv
 
     for line in reader:
         array[pointer] = line[0]
         pointer +=1
 
-print(str(array))
 
+insertionSort(array)
+
+for x in array:
+    print(x)
             
-
-bubbleSort(array) 
-
-print(str(array))
